5f9a668918ffed72ac192e5dad5dbf59c724faa3
Daniel Schmitz <danieljfa@gmail.com>
2015-11-26 14:24:05 -0200

    New babel version

--- CommentsComponent3/gulpfile.js
+++ CommentsComponent3/gulpfile.js
@@ -1,4 +1,3 @@
-
 var watchify = require('watchify');
 var browserify = require('browserify');
 var babelify = require('babelify');
@@ -11,31 +10,36 @@ var uglify = require('gulp-uglify');
 var sourcemaps = require('gulp-sourcemaps');
 var gutil = require('gulp-util');
 
-var b =  watchify(browserify({
-    entries: './src/index.jsx',
-    debug: true,
-    extensions: ['.jsx'],
-    transform: ['babelify']
-}));
 
-gulp.task('default', process);
-b.on('update', process);
+gulp.task('default', function(){
 
+    var b = watchify(browserify({
+        entries: './src/index.jsx',
+        debug: true,
+        extensions: ['.jsx']
+    }).transform("babelify", {presets: ["es2015", "react"]}));
 
-function process(){
+    function process(){
 
-     b.bundle()
-    .pipe(source('main.min.js'))
-    .pipe(buffer())
-    .pipe(sourcemaps.init({loadMaps: true}))
-    .pipe(uglify())
-    .on('error', gutil.log)
-    .pipe(sourcemaps.write('./'))
-    .pipe(gulp.dest('./public/js/'));
+        gutil.log("Building...");
 
-     gulp.src(["./node_modules/bootstrap/dist/css/bootstrap.min.css","./src/style.css"])
-    .pipe(concat('style.min.css'))
-    .pipe(uglifycss())
-    .pipe(gulp.dest('./public/css/'));    
+        gulp.src(["./node_modules/bootstrap/dist/css/bootstrap.min.css","./src/style.css"])
+        .pipe(concat('style.min.css'))
+        .pipe(uglifycss())
+        .pipe(gulp.dest('./public/css/'));
 
-}
+        return b.bundle()
+        .pipe(source('main.min.js'))
+        .pipe(buffer())
+        .pipe(sourcemaps.init({loadMaps: true}))
+        .pipe(uglify())
+        .on('error', gutil.log)
+        .pipe(sourcemaps.write('./'))
+        .pipe(gulp.dest('./public/js/'));
+     
+    }
+
+    b.on('update', process);
+    return process();
+
+});
